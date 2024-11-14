02be13ca38c2f91dbdc6a6daea062141ed20233e
Lorenzo Lima <123124446+Lorenzo-lima@users.noreply.github.com>
2024-11-13 23:31:48 -0300

    Update studentRoutes.js

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,15 +1,5 @@
 import express from 'express'
 import authMiddleware from '../middleware/authMiddleware.js'
-<<<<<<< HEAD
-import * as studentController from '../controllers/studentController.js'
-
-const router = express.Router()
-
-router.get('/', authMiddleware,)
-router.get('/:id', authMiddleware,)
-
-export default router
-=======
 import * as StudentController from '../controllers/studentController.js'
 
 const router = express.Router()
@@ -20,4 +10,4 @@ router.patch('/:id', authMiddleware, StudentController.updateStudent)
 router.delete('/:id', authMiddleware, StudentController.deleteStudent)
 
 export default router
->>>>>>> e0b1075df7cfad5d30426b50b5f60bdba06b8cc7
+
